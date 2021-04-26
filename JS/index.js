a9ecc1d8cd320205c9dff022e0607fefbc125b4f
changawa-antony <antony.changawa@student.moringaschool.com>
2021-04-26 06:47:49 +0300

    Updated js file and the other files

--- JS/index.js
+++ JS/index.js
@@ -17,11 +17,6 @@ $(document).ready(function() {
       
     });
 
-    $("#drink").click(function() {
-      $("#drink-type").toggle();
-      
-    });
-
     $("#del-point").click(function() {
       $("#del-station").toggle();
       
@@ -35,7 +30,6 @@ $(document).ready(function() {
       var pizzaSize = $("input[name='size']:checked").val();
       var crust = $("input[name='crust']:checked").val();
       var topping = $("input[name='topping']:checked").val();
-      var drink = $("input[name='drink']:checked").val();
 
       //checking for undefined input
 
@@ -54,18 +48,13 @@ $(document).ready(function() {
         return false;
       }
 
-      else if(drink == undefined){
-        alert("Please select the size of the pizza");
-        return false;
-      }
-
       else{
-        console.log(pizzaSize,crust,topping,drink)
+        console.log(pizzaSize,crust,topping)
 
       }
 
 
-      //Prise allocation
+      //Prise allocation for pizza
 
       if(pizzaSize == "small"){
         var price = 500;
@@ -79,23 +68,66 @@ $(document).ready(function() {
         var price = 950;
       }
 
+      //Prise allocation for crust
+
+      if(crust == "crispy"){
+        var crustPrice = 500;
+      }
+
+      if(crust == "stuffed"){
+        var crustPrice = 750;
+      }
+
+      if(crust == "glutten-free"){
+        var crustPprice = 950;
+      }
+
+      //Prise allocation for topping
+
+      if(topping == "pepperoni"){
+        var toppingPrice = 500;
+      }
+
+      if(topping == "mushroom & chicken"){
+        var toppingPrice = 750;
+      }
+
+      if(topping == "bacon"){
+        var toppingprice = 950;
+      }
+
+      if(topping == "extra-cheesy"){
+        var toppingPrice = 950;
+      }
+
+
+
       //initializing an object
 
-      function Order (pizzaSize, crust, topping, drink) {
+      function Order (pizzaSize, crust, topping) {
       this.orderSize = pizzaSize;
       this.orderCrust = crust;
       this.orderTopping = topping;
-      this.orderDrink = drink;
       }
 
-      var newOrder = new Order(pizzaSize, crust, topping, drink);
+      var newOrder = new Order(pizzaSize, crust, topping);
 
-      $("ul#order-display").append("<li><span>" + newOrder.orderSize + "</span> - <span> " + price + " </span></li>");
+      //displaying object data
+
+      $("div#order-display").append(
+        
+        "<div class='side'>" +
+        "<ul>" +
+        "<li><span>Pizza Size</span><span>" + newOrder.orderSize + "</span> - <span> " + price + " </span></li>" +
+        "<li><span>Pizza Crust</span><span>" + newOrder.orderCrust + "</span> - <span> " + price + " </span></li>" +
+        "<li><span>Pizza Topping</span><span>" + newOrder.orderTopping + "</span> - <span> " + price + " </span></li>" +
+        "</ul>" +
+        "</div>"
+      );
 
       $("input[name='size']:checked").val("");
       $("input[name='crust']:checked").val("");
       $("input[name='topping']:checked").val("");
-      $("input[name='drink']:checked").val("");
   
     });
 
